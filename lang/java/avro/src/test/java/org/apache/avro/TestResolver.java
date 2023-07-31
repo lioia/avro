95166a71deb331fde7e052759a30f1451fb9e8a3
Alessandro Lioi <lioialessandro2000@live.com>
2023-07-31 12:34:07 +0200

    test(Resolver): update dummy test to cover a few lines

--- lang/java/avro/src/test/java/org/apache/avro/TestResolver.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestResolver.java
@@ -1,6 +1,5 @@
 package org.apache.avro;
 
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.utils.ExpectedResult;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,26 +14,25 @@ public class TestResolver {
   private final ExpectedResult<Resolver.Action.Type> expected;
   private final Schema writer;
   private final Schema reader;
-  private final GenericData data;
 
   public TestResolver(TestParameters parameters) {
     this.expected = parameters.expected;
     this.writer = parameters.writer;
     this.reader = parameters.reader;
-    this.data = parameters.data;
   }
 
   @Parameterized.Parameters
   public static Collection<TestParameters> getParameters() {
     return Arrays.asList(
-      new TestParameters(new ExpectedResult<>(null, Exception.class), null, null, null)
+      new TestParameters(new ExpectedResult<>(null, Exception.class), null, null),
+      new TestParameters(new ExpectedResult<>(Resolver.Action.Type.DO_NOTHING, null), SchemaBuilder.builder().bytesType(), SchemaBuilder.builder().bytesType())
     );
   }
 
   @Test
   public void resolve() {
     try {
-      Resolver.Action result = Resolver.resolve(writer, reader, data);
+      Resolver.Action result = Resolver.resolve(writer, reader);
       Assert.assertEquals(expected.getResult(), result.type);
     } catch (Exception e) {
       Assert.assertNotNull(this.expected.getException());
@@ -45,13 +43,11 @@ public class TestResolver {
     private final ExpectedResult<Resolver.Action.Type> expected;
     private final Schema writer;
     private final Schema reader;
-    private final GenericData data;
 
-    public TestParameters(ExpectedResult<Resolver.Action.Type> expected, Schema writer, Schema reader, GenericData data) {
+    public TestParameters(ExpectedResult<Resolver.Action.Type> expected, Schema writer, Schema reader) {
       this.expected = expected;
       this.writer = writer;
       this.reader = reader;
-      this.data = data;
     }
   }
 }
