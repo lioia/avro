545989490266fed8ad409069cb88a379127fbc4e
Alessandro Lioi <lioialessandro2000@live.com>
2023-08-02 16:23:54 +0200

    test(DirectBinaryEncoder): test for write long, float and double
    Long is incomplete

--- lang/java/avro/src/test/java/org/apache/avro/io/TestDirectBinaryEncoder.java
+++ lang/java/avro/src/test/java/org/apache/avro/io/TestDirectBinaryEncoder.java
@@ -100,6 +100,110 @@ public class TestDirectBinaryEncoder {
     }
   }
 
+  @RunWith(Parameterized.class)
+  public static class TestWriteLong {
+    private final ExpectedResult<ByteBuffer> expected;
+    private final long parameter;
+    private final OutputStream output;
+
+    public TestWriteLong(TestParametersEncoder<Long> parameters) {
+      this.expected = parameters.expected();
+      this.parameter = parameters.parameter();
+      this.output = parameters.output();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<TestParametersEncoder<Long>> getParameters() {
+      byte[] byteMinus1 = new byte[]{0x01};
+      byte[] byte64 = new byte[]{(byte) 0x80, 0x01};
+      return Arrays.asList(
+        new TestParametersEncoder<>(new ExpectedResult<>(null, Exception.class), 0L, null),
+        new TestParametersEncoder<>(new ExpectedResult<>(ByteBuffer.wrap(byteMinus1), null), -1L, new ByteArrayOutputStream()),
+        new TestParametersEncoder<>(new ExpectedResult<>(ByteBuffer.wrap(byte64), null), 64L, new ByteArrayOutputStream())
+      );
+    }
+
+    @Test
+    public void readLong() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(output);
+        encoder.writeLong(parameter);
+        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) output;
+        Assert.assertArrayEquals(expected.getResult().array(), outputStream.toByteArray());
+      } catch (Exception e) {
+        Assert.assertNotNull(this.expected.getException());
+      }
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class TestWriteFloat {
+    private final ExpectedResult<ByteBuffer> expected;
+    private final float parameter;
+    private final OutputStream output;
+
+    public TestWriteFloat(TestParametersEncoder<Float> parameters) {
+      this.expected = parameters.expected();
+      this.parameter = parameters.parameter();
+      this.output = parameters.output();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<TestParametersEncoder<Float>> getParameters() {
+      byte[] byteMinus0dot1 = new byte[]{(byte) 0xCD, (byte) 0xCC, (byte) 0xCC, (byte) 0xBD};
+      return Arrays.asList(
+        new TestParametersEncoder<>(new ExpectedResult<>(null, Exception.class), 0f, null),
+        new TestParametersEncoder<>(new ExpectedResult<>(ByteBuffer.wrap(byteMinus0dot1), null), -0.1f, new ByteArrayOutputStream())
+      );
+    }
+
+    @Test
+    public void readFloat() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(output);
+        encoder.writeFloat(parameter);
+        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) output;
+        Assert.assertArrayEquals(expected.getResult().array(), outputStream.toByteArray());
+      } catch (Exception e) {
+        Assert.assertNotNull(this.expected.getException());
+      }
+    }
+  }
+
+  @RunWith(Parameterized.class)
+  public static class TestWriteDouble {
+    private final ExpectedResult<ByteBuffer> expected;
+    private final double parameter;
+    private final OutputStream output;
+
+    public TestWriteDouble(TestParametersEncoder<Double> parameters) {
+      this.expected = parameters.expected();
+      this.parameter = parameters.parameter();
+      this.output = parameters.output();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<TestParametersEncoder<Double>> getParameters() {
+      byte[] byteMinus0dot1 = new byte[]{(byte) 0x9A, (byte) 0x99, (byte) 0x99, (byte) 0x99, (byte) 0x99, (byte) 0x99, (byte) 0xB9, (byte) 0xBF};
+      return Arrays.asList(
+        new TestParametersEncoder<>(new ExpectedResult<>(null, Exception.class), 0.0, null),
+        new TestParametersEncoder<>(new ExpectedResult<>(ByteBuffer.wrap(byteMinus0dot1), null), -0.1, new ByteArrayOutputStream())
+      );
+    }
+
+    @Test
+    public void readDouble() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(output);
+        encoder.writeDouble(parameter);
+        ByteArrayOutputStream outputStream = (ByteArrayOutputStream) output;
+        Assert.assertArrayEquals(expected.getResult().array(), outputStream.toByteArray());
+      } catch (Exception e) {
+        Assert.assertNotNull(this.expected.getException());
+      }
+    }
+  }
+
   @RunWith(MockitoJUnitRunner.class)
   public static class TestWriteThrowOutputStream {
     @Mock
@@ -128,6 +232,39 @@ public class TestDirectBinaryEncoder {
       try {
         DirectBinaryEncoder encoder = new DirectBinaryEncoder(throwOutputStream);
         encoder.writeInt(1);
+        Assert.fail();
+      } catch (Exception ignored) {
+        // Success
+      }
+    }
+
+    @Test
+    public void writeLongThrowOutputStream() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(throwOutputStream);
+        encoder.writeLong(1L);
+        Assert.fail();
+      } catch (Exception ignored) {
+        // Success
+      }
+    }
+
+    @Test
+    public void writeFloatThrowOutputStream() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(throwOutputStream);
+        encoder.writeFloat(0.1f);
+        Assert.fail();
+      } catch (Exception ignored) {
+        // Success
+      }
+    }
+
+    @Test
+    public void writeDoubleThrowOutputStream() {
+      try {
+        DirectBinaryEncoder encoder = new DirectBinaryEncoder(throwOutputStream);
+        encoder.writeDouble(0.1);
         Assert.fail();
       } catch (Exception ignored) {
         // Success
